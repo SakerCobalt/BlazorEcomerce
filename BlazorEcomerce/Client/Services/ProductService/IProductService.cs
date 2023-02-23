16c82a2d8e2fe93be5dbe67077b4d102ee628d90
SakerCobalt <weston@ohiogray.org>
2023-02-22 21:03:05 -0500

    Seeding More Products

---
+++ BlazorEcomerce/Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorEcomerce.Shared;
+
+namespace BlazorEcomerce.Client.Services.ProductService
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+        Task<ServiceResponse<Product>> GetProductAsync(int productID);
+    }
+}
--- BlazorEcomerce/Server/Controllers/ProductController.cs
+++ BlazorEcomerce/Server/Controllers/ProductController.cs
@@ -8,17 +8,25 @@ namespace BlazorEcomerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly DataContext _context;
-        public ProductController(DataContext context)
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
         {
-            _context= context;
+            _productService = productService;
         }
 
             [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProduct()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
-            var Products = await _context.Products.ToListAsync();
-            return Ok(Products);
+            var result = await _productService.GetProductsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{productID}")]
+        //[Route("{id}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct(int productID)
+        {
+            var result = await _productService.GetProductAsync(productID);
+            return Ok(result);
         }
     }
 }
